d445b37bafbd28f34f9d1f23d738d33734591dd2
Michelle <shellayywow@gmail.com>
2026-05-12 10:18:09 -0500

    fix: Build is broken

--- src/components/Navigation.d.ts
+++ src/components/Navigation.d.ts
@@ -2,6 +2,8 @@ import type { Page } from '../types';
 type NavigationProps = {
     currentPage: Page;
     setCurrentPage: (page: Page) => void;
+    isDark: boolean;
+    toggleDark: () => void;
 };
-export default function Navigation({ currentPage, setCurrentPage }: NavigationProps): import("react/jsx-runtime").JSX.Element;
+export default function Navigation({ currentPage, setCurrentPage, isDark, toggleDark }: NavigationProps): import("react/jsx-runtime").JSX.Element;
 export {};
